7119aad9b98af36a6b17d2b2f3cdc643b47249bf
andresbrice <andres.martin.bricenio@gmail.com>
2021-09-05 18:29:19 -0300

    refactor user class

---
+++ src/User.java
@@ -0,0 +1,31 @@
+public class User {
+    private final double presupuesto;
+    private double tiempo;
+    private char atraccion;
+
+
+    public User(double presupuesto) {
+        this.presupuesto = presupuesto;
+        this.tiempo = tiempo;
+        this.atraccion = atraccion;
+
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public int getAtraccion() {
+        return atraccion;
+    }
+
+    public double calcularPresupuesto() {
+
+        return 0;
+    }
+}
+
