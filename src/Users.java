388839843d53a9e7b11cfa5cc6bc9aab4a7beaeb
CharlsHL <carlosermeti@hotmail.com>
2021-09-05 10:05:11 -0300

    "primer commit"

---
+++ src/Users.java
@@ -0,0 +1,25 @@
+public class Users {
+        private final double presupuesto;
+        private double tiempo = 0;
+        private char atraccion;
+
+
+    public Users(double presupuesto) {
+        this.presupuesto = presupuesto;
+        this.tiempo = tiempo;
+        this.atraccion= atraccion;
+
+    }
+
+    public double getPresupuesto() {
+            return presupuesto;
+        }
+        public double getTiempo() {
+            return tiempo;
+        }
+        public int getAtraccion() {
+            return atraccion;
+        }
+
+}
+
