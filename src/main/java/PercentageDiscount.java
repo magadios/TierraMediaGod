45fa9a24a7628ea4d5ccd0fe17bbbabbd42835fb
CharlsHL <carlosermeti@hotmail.com>
2021-09-15 14:51:39 -0300

    Promociones sumado el porcentage

--- src/main/java/PercentageDiscount.java
+++ src/main/java/PercentageDiscount.java
@@ -1,5 +1,36 @@
 public class PercentageDiscount implements DiscountService {
-  @Override
+  protected String typeOfAttraction;
+  protected int discount;
+  protected int atattractionCost;
+  protected Attraction attractionName1;
+  protected Attraction attractionName2;
+
+  public PercentageDiscount(String typeOfAttraction, Attraction attractionName1, Attraction attractionName2, int discount) {
+    super();
+    this.discount = discount;
+    this.typeOfAttraction = typeOfAttraction;
+    this.attractionName1=attractionName1;
+    this.attractionName2=attractionName2;
+  }
+
+  public String getPromo() {
+    return attractionName1.getAttractionName() + " + " + attractionName2.getAttractionName() +" por "+ discount + " monedas!";
+  }
+
+  public String getTypeOfAttraction() {
+    return typeOfAttraction;
+  }
+
+  protected double calculateCost() {
+   atattractionCost = Math.round((attractionName1.getAttractionCost()+ attractionName2.getAttractionCost()) * (100 - discount)/100);
+    return atattractionCost;
+  }
+
+  public String toString() {
+    return "Promo descuento %  [tipo = " + typeOfAttraction + ", descuento= " + discount + " Porciento , coste final =" + atattractionCost
+            + attractionName1.getAttractionCost() + " + " + attractionName2.getAttractionCost() + "]";
+  }
+
   public double getDiscount() {
     return 0;
   }
